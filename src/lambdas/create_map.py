0527a661d4c12fb8aa2f766642bd38b36319dc7c
Dinis Cruz <dinis.cruz@owasp.org>
2018-11-26 06:09:34 +0000

    improved Lambda

--- src/lambdas/create_map.py
+++ src/lambdas/create_map.py
@@ -2,26 +2,22 @@ import asyncio
 import base64
 import os
 import subprocess
+from distutils.dir_util import copy_tree
 from time import sleep
 
+from utils.Files import Files
 from utils.Process import Process
 from utils.aws.Lambdas import load_dependency
 
+path_headless_shell          = '/tmp/lambdas-dependencies/pyppeteer/headless_shell'     # path to headless_shell AWS Linux executable
+path_page_screenshot         = '/tmp/screenshot.png'                                    # path to store screenshot of url loaded
+os.environ['PYPPETEER_HOME'] = '/tmp'                                                   # tell pyppeteer to use this read-write path in Lambda aws
+target_url                   = 'http://localhost:1234/map/simple'
+web_root                     = '/tmp/html'
 
-def run(event, context):
+def open_browser_and_take_screenshot():
+
     load_dependency('pyppeteer')
-
-    path_headless_shell          = '/tmp/lambdas-dependencies/pyppeteer/headless_shell'     # path to headless_shell AWS Linux executable
-    path_page_screenshot         = '/tmp/screenshot.png'                                    # path to store screenshot of url loaded
-    os.environ['PYPPETEER_HOME'] = '/tmp'                                                   # tell pyppeteer to use this read-write path in Lambda aws
-
-    web_root =  './html'
-
-    proc = subprocess.Popen(["python", "-m", "http.server", "1234"], cwd=web_root, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    sleep(0.5)
-
-    target_url = 'http://localhost:1234/map/simple'
 
     async def take_screenshot():
         from pyppeteer import launch                                                        # import pyppeteer dependency
@@ -32,18 +28,43 @@ def run(event, context):
         page = await browser.newPage()                                                      # typical pyppeteer code, where we create a new Page object
         await page.goto(target_url)                                                         #   - open an url
 
-        await page.waitFor(2 * 1000);
+        #await page.waitFor(2 * 1000);
 
-        #await page.screenshot({'path': path_page_screenshot})                               # - take a screenshot of the page loaded and save it
-        await page.pdf({'path': path_page_screenshot});
+        await page.screenshot({'path': path_page_screenshot, 'fullPage': True})             # - take a screenshot of the page loaded and save it
+        #await page.pdf({'path': path_page_screenshot});
         await browser.close()
 
     asyncio.get_event_loop().run_until_complete(take_screenshot())
-    proc.kill()
 
+def get_file_data():
     with open(path_page_screenshot, "rb") as image_file:                                    # open path_page_screenshot file
         encoded_png =  base64.b64encode(image_file.read()).decode()                         # save it as a png string (base64 encoded to make it easier to return)
-
     return { "base64_data" : encoded_png}                                                   # return value to Lambda caller
 
-    #return proc.stderr.read().decode()
+def get_page_html():
+    load_dependency('requests')   ; import requests
+    return requests.get(target_url).text     # 'http://localhost:1234/map/'
+
+def setup_tmp_web_root(payload):
+    copy_tree('./html', web_root)
+    cs_map_1 = web_root + '/coffee/map-1.coffee'
+    cs_code = payload['coffee_script_code']
+    Files.write(cs_map_1, cs_code)
+    return Files.contents(cs_map_1)
+
+def run(event, context):
+    setup_tmp_web_root(event)
+
+    proc = subprocess.Popen(["python", "-m", "http.server", "1234"], cwd=web_root, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    sleep(0.5)
+
+    #html = get_page_html()
+    open_browser_and_take_screenshot()
+
+    proc.kill()
+
+    return get_file_data()
+
+
+    #return html
--- test/lambdas/Test_lambda_create_map.py
+++ test/lambdas/Test_lambda_create_map.py
@@ -19,14 +19,17 @@ class Test_lambda_create_map(TestCase):
         self.folder_src_lambdas = Files.path_combine(__file__, '../../../src/lambdas')
         self.folder_src_hugo    = Files.path_combine(__file__, '../../../src/hugo')
 
-    def zip_update_invoke(self):
+    def zip_update_invoke(self, payload = {}):
         self.copy_html_files()
-        _lambda = Lambdas('create_map', 'create_map.run')
+        _lambda = Lambdas('create_map', 'create_map.run',memory = 3008).delete().create()
         copy_tree(_lambda.source         , self.src_tmp)
         copy_tree(self.folder_src_lambdas, self.src_tmp)
         _lambda.source = self.src_tmp
-        return _lambda.update().invoke()
+        return _lambda.update().invoke(payload)
 
+    def just_invoke(self, payload):
+        _lambda = Lambdas('create_map', 'create_map.run')
+        return _lambda.invoke(payload)
 
     def copy_html_files(self):
         html_file = self.folder_src_hugo + '/layouts/visjs/simple.html'
@@ -40,44 +43,46 @@ class Test_lambda_create_map(TestCase):
         Files.copy(cs_vis_js    , self.src_tmp + '/html/coffee/api-visjs.coffee')
         Files.copy(cs_map_1     , self.src_tmp + '/html/coffee/map-1.coffee')
 
-    @sync
-    async def take_screenshot(self):
-        (_,_,_,browser) = await API_Browser().open('http://localhost:1234/map/simple')
-        return await browser.screenshot()
+
+    def save_image(self, result, png_file):
+        png_data = result.get('base64_data')
+        if (png_data):
+            with open(png_file, "wb") as fh:
+                fh.write(base64.decodebytes(png_data.encode()))
+            Dev.pprint('Image saved to {0}'.format(png_file))
+        else:
+            Dev.pprint(result)
 
     def test_invoke(self):
-        result = self.zip_update_invoke()
+        #result = self.zip_update_invoke()
+        result = self.just_invoke()
         #Show_Img.from_png_string(result['base64_data'])
         pdf_data = result['base64_data']
         # Dev.pprint(len(pdf_data))
         with open('./lambda-result.pdf', "wb") as fh:
             fh.write(base64.decodebytes(pdf_data.encode()))
 
+    def test_invoke_using_map_as_param(self):
+        cs_map_2 = self.folder_src_hugo + '/static/coffee/map-2.coffee'
+        payload = { 'coffee_script_code' : Files.contents(cs_map_2) }
+        #result  = self.zip_update_invoke(payload)
+        result = self.just_invoke(payload)
 
-    def test_create_and_invoke(self):
-        #Files.folder_delete_all(self.src_tmp)
+        self.save_image(result, './lambda-result.png')
+
+    @sync
+    async def take_screenshot(self):
+        (_, _, _, browser) = await API_Browser().open('http://localhost:1234/map/simple')
+        return await browser.screenshot()
+
+    def test_create_local_server_and_take_screenshot(self):
+        Files.folder_delete_all(self.src_tmp)
         self.copy_html_files()
 
-
-
         web_root = self.src_tmp + '/html'
-
         proc = subprocess.Popen(["python","-m", "SimpleHTTPServer","1234"], cwd = web_root)
-        #proc_thread = threading.Thread (target = Process.run , args=("python",["-m", "SimpleHTTPServer","1234"], web_root))
-        #proc_thread.start()
-
-        #sleep(1000)
         Dev.pprint(proc)
-
-
-        #from time import sleep
-        #sleep(3000)
-
-        #self.take_screenshot()
+        self.take_screenshot()
         html =  requests.get('http://localhost:1234/map/').text
         proc.kill()
-
-        #Dev.pprint(self.zip_update_invoke())
-
-
         Dev.pprint(html)
